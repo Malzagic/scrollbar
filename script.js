493aa762dfd6fd8886acd38355a758950f9698b2
Malzagic <przemek2142@gmail.com>
2022-06-22 18:13:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const scrollToTop = document.querySelector('.scroll-to-top');
+const root = document.querySelector(':root');
+
+
+const showScrollHeight = () => {
+
+  const scrollHeight = window.scrollY;
+  const letToScroll = document.body.getBoundingClientRect().height - window.innerHeight;
+  // console.log(`wysokość viewportu: ${window.innerHeight}`);
+  // console.log(`wysokość strony: ${document.body.getBoundingClientRect().height}`);
+
+  const scrollBarWidth = Math.floor((scrollHeight / letToScroll) * 100);
+
+  root.style.setProperty('--scroll-width', scrollBarWidth + '%');
+  if(scrollBarWidth > 100){
+    scrollToTop.classList.add('active');
+  } else {
+    scrollToTop.classList.remove('active');
+  }
+}
+
+window.addEventListener('scroll', showScrollHeight);
